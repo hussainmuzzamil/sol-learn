2abc1089cba1108d327fca77fdb5614da0359d1f
1sudo <cc.muzz@gmail.com>
2021-12-16 23:14:38 +0500

    Playing around with import function to add multiple contracts from a single one

--- oz-learn/contracts/myToken.sol
+++ oz-learn/contracts/myToken.sol
@@ -2,10 +2,14 @@
 
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC721";
+import "@openzeppelin/contracts/token/ERC777";
+import "@openzeppelin/contracts/token/ERC1155";
 
 
-contract myToken is ERC20 {
+contract myToken is ERC20, ERC721, ERC777, ERC1150 {
     
 
 }
